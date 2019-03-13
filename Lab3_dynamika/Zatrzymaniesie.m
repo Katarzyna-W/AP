09dcd425a62b5d089e5b264a69f532c24680ea35
Unknown <katarzyna.watorska@gmail.com>
2019-03-13 16:57:50 +0100

    sprawko2 - opis problemu i rozwiązanie

--- Lab3_dynamika/Zatrzymaniesie.m
+++ Lab3_dynamika/Zatrzymaniesie.m
@@ -1,9 +1,11 @@
  load('rec1_20kph_break0.mat');
- h = 0.001; m=350;
- data1 = iddata(rec1_013.speed', [], 0.001);
+ h = 0.001; %czas pomiêdzy pomiarami
+ m=350; %masa samochodu
+ data1 = iddata(rec1_013.speed', [], h);
  model1 = armax(data1, [1 1]);
- a1=getpvec(model1);
+ a1 = getpvec(model1);
  compare(data1,model1);
- bf=(a1(1)+1)*m/h
+ %wspó³czynnik tarcia
+ bf = (a1(1)+1)*m/h;
  
  
--- Lab3_dynamika/ruszsie.m
+++ Lab3_dynamika/ruszsie.m
@@ -1,9 +1,12 @@
  load('rec1_gas0-100.mat');
- h = 0.001; m=350;
- data2 = iddata(rec1_006.speed', rec1_006.force', 0.001);
+ h = 0.001; %czas pomiêdzy pomiarami
+ m=350; %masa samochodu
+ data2 = iddata(rec1_006.speed', rec1_006.force', h);
  model2 = armax(data2, [1 1 0 1]);
- a1=getpvec(model2)
+ a1 = getpvec(model2);
  compare(data1,model1);
- bf=(a1(1)+1)*m/h
- ba=a1(2)*m/h
+ %wspó³czynnik tarcia
+ bf = (a1(1)+1)*m/h;
+ %wspó³czynnik hamowania
+ ba = a1(2)*m/h;
  
