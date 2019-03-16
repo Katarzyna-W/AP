262a0cc1da9c1fda5e917735fd3a032f603a962c
Bartłomiej Mróz <bartlomiej.mroz@poczta.onet.pl>
2019-03-16 18:45:11 +0100

    Jakieś tam wnioski i poprawki

--- Lab3_dynamika/ruszsie.m
+++ Lab3_dynamika/ruszsie.m
@@ -4,7 +4,7 @@
  data2 = iddata(rec1_006.speed', rec1_006.force', h);
  model2 = armax(data2, [1 1 0 1]);
  a1 = getpvec(model2);
- compare(data1,model1);
+ compare(data2,model2);
  %wspó³czynnik tarcia
  bf = (a1(1)+1)*m/h;
  %wspó³czynnik hamowania
