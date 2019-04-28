89b9a8dc51ea70864a166ccd4b1f070ed1283200
Bartłomiej Mróz <bartlomiej.mroz@poczta.onet.pl>
2019-04-28 22:58:04 +0200

    Ogarnięcie fminsearch

--- Lab9_zawieszenie/optymalizacja.m
+++ Lab9_zawieszenie/optymalizacja.m
@@ -1,6 +1,11 @@
-function blad = optymalizacja(in)
-
-blad = (in(2)-0.1).^2;
+function [MSE] = optymalizacja(in)
+alfa = in(1);
+beta = in(2);
+k = in(3);
+model = sim('model','SimulationMode','normal');
+signal = model.get('simout').Data;
+step = model.get('simout1').Data;
+MSE = sum((signal-step).^2);
 end
 
 
--- Lab9_zawieszenie/zawieszenie_dane.m
+++ Lab9_zawieszenie/zawieszenie_dane.m
@@ -4,13 +4,11 @@ cc = 15000;
 cw = 200000;
 dc = 200;
 dw = 10;
-alfa=10;
-beta=10;
-k=20;
-
-a = sim('model','SimulationMode','normal');
-b = a.get('simout');
-time = b.Time;
-value = b.Data;
-    
-blad = fminsearch(@optymalizacja,[time(1), value(1)])
+alfa = 7;
+beta = 23;
+k = 8;
+options = optimset('MaxFunEvals',40000);
+opti = fminsearch('optymalizacja',[alfa, beta, k],options);
+alfa = opti(1);
+beta = opti(2);
+k = opti(3);
